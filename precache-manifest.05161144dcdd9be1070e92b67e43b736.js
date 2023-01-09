1e35ecc530027dc66167b9341241190fc4fbf730
Yuxiang-Kang <yxkang1912923753@gmail.com>
2023-01-09 03:34:40 -0800

    Updates

---
+++ precache-manifest.05161144dcdd9be1070e92b67e43b736.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e11f7b43481450b8615",
+    "url": "/Portfolio-Yuxiang/static/css/main.876fec28.chunk.css"
+  },
+  {
+    "revision": "4e11f7b43481450b8615",
+    "url": "/Portfolio-Yuxiang/static/js/main.4e11f7b4.chunk.js"
+  },
+  {
+    "revision": "8661c4af1aa50a4cec73",
+    "url": "/Portfolio-Yuxiang/static/js/1.8661c4af.chunk.js"
+  },
+  {
+    "revision": "0e6b2b30338a1e24e389",
+    "url": "/Portfolio-Yuxiang/static/css/2.46be3a0f.chunk.css"
+  },
+  {
+    "revision": "0e6b2b30338a1e24e389",
+    "url": "/Portfolio-Yuxiang/static/js/2.0e6b2b30.chunk.js"
+  },
+  {
+    "revision": "f6a22d62937911404a51",
+    "url": "/Portfolio-Yuxiang/static/js/3.f6a22d62.chunk.js"
+  },
+  {
+    "revision": "a9a576e97fb998f6bb81",
+    "url": "/Portfolio-Yuxiang/static/js/runtime~main.a9a576e9.js"
+  },
+  {
+    "revision": "f4376b9f7163e3db0c078ee41eab3a13",
+    "url": "/Portfolio-Yuxiang/static/media/Resume - Yuxiang Kang-4.f4376b9f.pdf"
+  },
+  {
+    "revision": "bc2c7afa280d7a7d54e2c5ecf515e8a6",
+    "url": "/Portfolio-Yuxiang/static/media/home-bg.dc52d5d4.jpg"
+  },
+  {
+    "revision": "828cbbbbc974d32ac58d2e5a1b56882e",
+    "url": "/Portfolio-Yuxiang/static/media/pre.828cbbbb.svg"
+  },
+  {
+    "revision": "1375105252bcf77e2c008c9557d1aa8e",
+    "url": "/Portfolio-Yuxiang/static/media/home-projects.8a937b0e.png"
+  },
+  {
+    "revision": "c5230de551ad89dbe393afd31a60a6a2",
+    "url": "/Portfolio-Yuxiang/static/media/Me.3c91db45.png"
+  },
+  {
+    "revision": "b4f6d9311f3ccfbca4a6f514cf143b3b",
+    "url": "/Portfolio-Yuxiang/static/media/SolidWorks-Logo.5a0c140d.png"
+  },
+  {
+    "revision": "5112f99f7294466cfcc3bc98631980cd",
+    "url": "/Portfolio-Yuxiang/static/media/Autocad-Logo.59056a9b.png"
+  },
+  {
+    "revision": "2e0de1db55b54c0a390be4a1c10f6aca",
+    "url": "/Portfolio-Yuxiang/static/media/CREO-Logo.964dc79b.png"
+  },
+  {
+    "revision": "68973553b5d7a937a9b5c50b72706b6d",
+    "url": "/Portfolio-Yuxiang/static/media/CATIA-Logo.1f3f2024.png"
+  },
+  {
+    "revision": "d36741dc2227ff5ddd3e00fce7888d1e",
+    "url": "/Portfolio-Yuxiang/static/media/ANSYS-Logo.eeafadb8.png"
+  },
+  {
+    "revision": "a5f1f495a4398231413210ec99f485e7",
+    "url": "/Portfolio-Yuxiang/static/media/abaqus-Logo.a2a295cb.png"
+  },
+  {
+    "revision": "90f97fe537fee40cebcd8a5c1934b3dd",
+    "url": "/Portfolio-Yuxiang/static/media/comsol-Logo.744a3404.png"
+  },
+  {
+    "revision": "96c63c459e38c21960abb80ffe1e209e",
+    "url": "/Portfolio-Yuxiang/static/media/matlab-Logo.4cedb75e.png"
+  },
+  {
+    "revision": "c512d9da504be4d96313504211775d4e",
+    "url": "/Portfolio-Yuxiang/static/media/leaf.ff2616f6.png"
+  },
+  {
+    "revision": "750c7d884d31442e823bce13781275b9",
+    "url": "/Portfolio-Yuxiang/static/media/emotion.eabfc16e.png"
+  },
+  {
+    "revision": "408d49ba10562bd34236c63d4e6af221",
+    "url": "/Portfolio-Yuxiang/static/media/codeEditor.1b585492.png"
+  },
+  {
+    "revision": "44190220743b70a49855d73399eec8d8",
+    "url": "/Portfolio-Yuxiang/static/media/chatify.7206044e.png"
+  },
+  {
+    "revision": "22cb85719800191ea12a9361fbfef9b2",
+    "url": "/Portfolio-Yuxiang/static/media/suicide.9c86cd71.png"
+  },
+  {
+    "revision": "69948a28ccbd01fdd576340358088e37",
+    "url": "/Portfolio-Yuxiang/static/media/blog.fb8ab7f2.png"
+  },
+  {
+    "revision": "4083c54fc6ef2ff4f888e22c6ba99ca2",
+    "url": "/Portfolio-Yuxiang/static/media/Plugging-3.b9414117.png"
+  },
+  {
+    "revision": "6a9db96a3dfd47d06686fc5c8584155f",
+    "url": "/Portfolio-Yuxiang/static/media/fish-1.a74081a7.png"
+  },
+  {
+    "revision": "14b20a63824215167ee9a9c4c09e43fc",
+    "url": "/Portfolio-Yuxiang/static/media/car-1.690eaa7f.png"
+  },
+  {
+    "revision": "f581b686ce98808636dca3af6ded8d27",
+    "url": "/Portfolio-Yuxiang/index.html"
+  }
+];
