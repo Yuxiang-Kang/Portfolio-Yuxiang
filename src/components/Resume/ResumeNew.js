58c80a2ffcfe68504ba4f860291157f132b8bcaa
Yuxiang-Kang <120524636+Yuxiang-Kang@users.noreply.github.com>
2023-02-15 15:56:57 -0800

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,6 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/Resume - Yuxiang Kang-4.pdf";
+import pdf_1 from "../../Assets/../Assets/Resume_Sean Kang_6.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -24,7 +25,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={pdf1}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
